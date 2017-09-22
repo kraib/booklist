a1a6dc651333458e69f3b0ee3acc137a4084e558
Adam Rackis <adamrackis@hotmail.com>
2017-09-22 09:55:24 -0500

    adding a few other types

--- resolvers.js
+++ resolvers.js
@@ -8,12 +8,32 @@ const books = [
     title: "Book 2"
   }
 ];
+
+const authors = [
+  {
+    _id: "1",
+    name: "Adam"
+  },
+  {
+    _id: "2",
+    name: "Bob"
+  }
+];
 module.exports = {
   Query: {
     books(root, args, context, info) {
-      console.log(info.fieldNodes.find(fn => fn.kind == "Field").selectionSet.selections.map(selection => selection.name.value));
+      debugger;
+      console.log("BOOKS");
+      //console.log(info.fieldNodes.find(fn => fn.kind == "Field").selectionSet.selections.map(selection => selection.name.value));
       //console.log(info.operation.selectionSet.selections.map(selection => selection.name.value));
       return books;
+    },
+    authors(root, args, context, info) {
+      debugger;
+      console.log("AUTHORS");
+      //console.log(info.fieldNodes.find(fn => fn.kind == "Field").selectionSet.selections.map(selection => selection.name.value));
+      //console.log(info.operation.selectionSet.selections.map(selection => selection.name.value));
+      return authors;
     }
   }
 };
--- schema.js
+++ schema.js
@@ -2,15 +2,30 @@ const resolvers = require("./resolvers");
 const { makeExecutableSchema, addMockFunctionsToSchema } = require("graphql-tools");
 
 const typeDefs = `
+
+type Author {
+    _id: String!
+    name: String
+    address: Address
+}
+
+type Address {
+    _id: String!
+    authors: [Author]
+}
+
 type Book {
-   _id: String!                # "!" denotes a required field
+   _id: String!
    title: String
    publisher: String
+   author(first: Int): Author
 }
+
 # This type specifies the entry points into our API. In this case
 # there is only one - "books" - which returns a list of books.
 type Query {
-   books(title: String, _id: String!): [Book]    # "[]" means this is a list of Books
+   books(title: String, _id: String): [Book]    
+   authors(name: String, _id: String): [Author]
 }
 `;
 
